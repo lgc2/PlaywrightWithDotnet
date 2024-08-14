f756e0a190fd8ca57d478bc35fc2af6a521b4dd9
lgc2 <lgustavoccarvalho@gmail.com>
2024-08-14 16:37:55 -0300

    feat(ApiTests): implements delete product test

--- EaApplicationTest/ApiTests/ProductsApiTests.cs
+++ EaApplicationTest/ApiTests/ProductsApiTests.cs
@@ -2,7 +2,7 @@
 using EaApplicationTest.Models;
 using EaFramework.Driver;
 using FluentAssertions;
-using FluentAssertions.Execution;
+using Microsoft.Playwright;
 
 namespace EaApplicationTest.ApiTests;
 
@@ -16,9 +16,11 @@ public class ProductsApiTests
 	}
 
 	[Fact]
-	public async Task GetProduct()
+	public async Task GetProductTest()
 	{
-		var productResponse = await (await _playwrightDriver.ApiRequestContext).GetAsync("Product/GetProductById/1");
+		var productCreated = await CreateProductAsync();
+
+		var productResponse = await (await _playwrightDriver.ApiRequestContext).GetAsync($"Product/GetProductById/{productCreated?.Id}");
 		var data = await productResponse.JsonAsync();
 
 		var product = data?.Deserialize<Product>(new JsonSerializerOptions
@@ -26,12 +28,59 @@ public class ProductsApiTests
 			PropertyNameCaseInsensitive = true
 		});
 
-		using (new AssertionScope())
+		productResponse.Status.Should().Be(200);
+		product?.Name.Should().Be(productCreated?.Name);
+		product?.Price.Should().Be(productCreated?.Price);
+		product?.ProductType.Should().Be(productCreated?.ProductType);
+		product?.Id.Should().Be(productCreated?.Id);
+	}
+
+	[Fact]
+	public async Task DeleteProductTest()
+	{
+		var product = await CreateProductAsync();
+
+		var deleteResponse = await (await _playwrightDriver.ApiRequestContext).DeleteAsync("Product/Delete",
+			new APIRequestContextOptions()
+			{
+				Params = new Dictionary<string, object>
+				{
+					{ "id", $"{product?.Id}" }
+				}
+			});
+
+		var getResponse = await (await _playwrightDriver.ApiRequestContext).GetAsync($"Product/GetProductById/{product?.Id}");
+
+		deleteResponse.Status.Should().Be(200);
+		getResponse.Status.Should().Be(204);
+	}
+
+	public async Task<Product?> CreateProductAsync()
+	{
+		var productData = new Product()
 		{
-			productResponse.Status.Should().Be(200);
-			product?.Name.Should().Be("Keyboard");
-			product?.Price.Should().Be(150);
-			product?.Id.Should().Be(1);
-		}
+			Name = "Test product",
+			Description = "Test description",
+			Price = 7895,
+			ProductType = ProductType.EXTERNAL
+		};
+
+		var createResponse = await (await _playwrightDriver.ApiRequestContext).PostAsync("/Product/Create",
+			new APIRequestContextOptions()
+			{
+				Headers = new Dictionary<string, string>
+				{
+					{ "accept", "text/plain" },
+					{ "Content-Type", " application/json-patch+json" }
+				},
+				DataObject = productData
+			});
+
+		var createData = await createResponse.JsonAsync();
+
+		return createData?.Deserialize<Product>(new JsonSerializerOptions
+		{
+			PropertyNameCaseInsensitive = true
+		});
 	}
 }
